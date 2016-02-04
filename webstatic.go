a20df2090639d3bda8132d798d48fec8ee1a2db2
JohnnyDevOps <john@xonk.org>
2016-02-04 16:07:12 -0700

    adding web autolaunch on run

--- webstatic.go
+++ webstatic.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	// relative path to the statik bin data
 	_ "./statik"
 	//_ "github.com/eyetoe/webstatic/statik"
 	"github.com/rakyll/statik/fs"
+	"os/exec"
+	"runtime"
 )
 
 // Before buildling, run `statik -src=./data`
@@ -23,5 +26,21 @@ func main() {
 	// Serve the data from the ./assets/ also for example
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(statikFS)))
 
+	openURL("http://localhost:8080/")
 	http.ListenAndServe(":8080", nil)
+}
+
+func openURL(url string) error {
+	var err error
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", "http://localhost:4001/").Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("Cannot open URL %s on this platform", url)
+	}
+	return err
 }
